f48f20b51453ac477cf38f8003d60f543224d839
sofiesana <s.sananikone@student.rug.nl>
2024-05-23 09:49:45 +0200

    debugging

--- train.py
+++ train.py
@@ -117,6 +117,7 @@ def train(net, dataset, criterion, optimizer, scheduler, train_loader, test_load
                         if best_test_loss is None:
                             best_test_loss = test_loss
                         elif test_loss < best_test_loss:
+                            print("best model saved at:", f"{dest}/best_{network_name}.pth")
                             torch.save(net.state_dict(),
                                        f"{dest}/best_{network_name}.pth")
                             best_test_loss = test_loss
