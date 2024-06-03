ebfb1c60c81afe00140690b28959e12f8df91523
sofiesana <s.sananikone@student.rug.nl>
2024-06-03 12:39:35 +0200

    updated comparison

--- irof_seg_layer_comparison.py
+++ irof_seg_layer_comparison.py
@@ -221,6 +221,80 @@ def plot_avg_irof_curve_sn(histories, layer_name):
     plt.savefig(path)
     plt.close()
 
+def get_resized_binary_mask(img_names, preds, class_name, class_category):
+    resized_preds = F.interpolate(preds, (480, 640))
+    normalized_masks = torch.nn.functional.softmax(resized_preds, dim=1)
+    class_mask = normalized_masks.argmax(axis=1).detach().cpu().numpy()
+    class_mask_uint8 = 255 * np.uint8(class_mask == class_category)
+
+    for i in range(len(class_mask_uint8)):
+        name = img_names[i]
+        img = class_mask_uint8[i, :, :, None]
+        masked_pixels = Image.fromarray(np.repeat(img, 3, axis=-1))
+
+        path = os.path.join(RESULTS_ROOT, class_name, name+'_predicted_mask.jpg')
+        masked_pixels.save(path)
+
+def get_gradcam_image(img_names, attributions, image, class_name, layer_name):
+    for i in range(len(image)):
+        name = img_names[i]
+        og_img = (image[i].cpu().squeeze().permute(1,2,0).numpy())
+        og_img = (og_img - og_img.min()) / (og_img.max() - og_img.min())
+        
+        cam_image = show_cam_on_image(og_img, attributions[i], use_rgb=True)
+
+        cam_image_final = Image.fromarray(cam_image)
+        path = os.path.join(RESULTS_ROOT, class_name, f"{name}_grad_cam_{layer_name}.jpg")
+        cam_image_final.save(path)
+
+def get_attributions(model, class_category, class_mask_float, image, layer):
+    target_layers = [layer]
+    targets = [SemanticSegmentationTarget(class_category, class_mask) for class_mask in class_mask_float]
+    with GradCAM(model=model, target_layers=target_layers) as cam:
+        grayscale_cam = cam(input_tensor=image,
+                            targets=targets)
+    return grayscale_cam
+
+def get_binary_mask(preds, class_category):
+    normalized_masks = torch.nn.functional.softmax(preds, dim=1)        
+    class_mask = normalized_masks.argmax(axis=1).detach().cpu().numpy()
+    class_mask_float = np.float32(class_mask == class_category)
+    return class_mask_float
+
+def plot_all_irof_curves(histories, class_name, layer_name):
+    for history in histories:
+        history = np.array(history)
+        plt.plot(range(len(history)), history, marker='o')
+    plt.title('AOC Curve')
+    plt.xlabel('Number of Segments Removed')
+    plt.ylabel('Class ' + class_name + ' Score')
+    plt.grid(True)
+    path = os.path.join(RESULTS_ROOT, class_name, f"all_irof_{layer_name}.jpg")
+    plt.savefig(path)
+    plt.close()
+
+def plot_avg_irof_curve(histories, class_name, layer_name):
+    # Step 1: Find the length of the longest list
+    max_length = max(len(lst) for lst in histories)
+
+    # Step 2: Pad shorter lists with NaN values
+    padded_lists = [lst + [np.nan] * (max_length - len(lst)) for lst in histories]
+
+    # Step 3: Convert to a NumPy array
+    array = np.array(padded_lists)
+
+    # Step 4: Compute the mean along axis 0, ignoring NaN values
+    avg_curve = np.nanmean(array, axis=0)
+
+    plt.plot(range(len(avg_curve)), avg_curve, marker='o')
+    plt.title('AOC Curve')
+    plt.xlabel('Number of Segments Removed')
+    plt.ylabel('Class ' + class_name + ' Score')
+    plt.grid(True)
+    path = os.path.join(RESULTS_ROOT, class_name, f"avg_irof_{layer_name}.jpg")
+    plt.savefig(path)
+    plt.close()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='IROF Evaluation')
     parser.add_argument(
@@ -254,18 +328,22 @@ if __name__ == "__main__":
     all_scores = {}
     all_histories = {}
 
+    sem_idx_to_class = {idx: cls for (idx, cls) in enumerate(CLASS_NAMES)}
+    sem_class_to_idx = {cls: idx for (idx, cls) in enumerate(CLASS_NAMES)}
+
     layers_to_try = [model.backbone, model.backbone.blocks[3][2].relu, model.backbone.blocks[3][2].conv2]  # Example layers
     layer_names = ["backbone", "blocks[3][2].relu", "blocks[3][2].conv2"]
 
     for layer, layer_name in zip(layers_to_try, layer_names):
-        all_histories[layer_name] = []
-        all_scores[layer_name] = []
+        class_scores = {}
+        class_histories = {}
 
         os.mkdir(os.path.join(RESULTS_ROOT, layer_name))
         test_loader = DataLoader(test_dataset, batch_size=10,
                              num_workers=8, shuffle=False, pin_memory=True)
+        
         for i, gt_batch in enumerate(test_loader):
-            
+        
             model.eval()
             gt_batch["img"] = Variable(gt_batch["img"]).to(device)
             if "seg" in gt_batch:
@@ -276,8 +354,6 @@ if __name__ == "__main__":
                 gt_batch["normal"] = Variable(gt_batch["normal"]).to(device)
 
             preds = model(gt_batch["img"])
-            print(preds)
-            print(F.normalize(preds, dim=1))
             
             img_names = gt_batch["name"]
             image = gt_batch["img"]
@@ -285,42 +361,87 @@ if __name__ == "__main__":
             for i in range(len(image)):
                 img_names[i] = str(img_names[i]).replace('.png', '')
             
-            irof = quantus.IROF(segmentation_method="slic",
-                                    perturb_baseline=irof_version,
-                                    perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
-                                    return_aggregate=False,
-                                    class_category=None,
-                                    class_name=None,
-                                    num_classes=40,
-                                    task = task
-                                    )
+            for class_category in range(TASKS_NUM_CLASS[0]):
 
-            attributions = get_sn_attributions(model, image, layer)
-            get_gradcam_image_sn(img_names, attributions, image, layer_name)
-            get_sn_image(img_names, preds)
+                class_name = sem_idx_to_class[class_category]
 
-            scores, histories = irof(model=model,
-                    x_batch=gt_batch["img"],
-                    y_batch=preds,
-                    a_batch=attributions,
-                    device=device)
-            
-            if scores is not None:
-                all_scores[layer_name].extend(scores)
-                all_histories[layer_name].extend(histories)
-            break
+                irof = quantus.IROF(segmentation_method="slic",
+                                        perturb_baseline=irof_version,
+                                        perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
+                                        return_aggregate=False,
+                                        class_category=class_category,
+                                        class_name=class_name,
+                                        num_classes=40, 
+                                        task = task
+                                        )
+
+                class_mask_float = get_binary_mask(preds, class_category)
+                attributions = get_attributions(model, class_category, class_mask_float, image, layer)
+                
+                valid_indices = []
+
+                # i is img index in batch
+                for i, img_seg in enumerate(torch.argmax(preds, axis=1)):
+                    if class_category not in img_seg:
+                        print(class_name, " not in image ", str(img_names[i]))
+                    if np.all((attributions[i] == 0)):
+                        print("attributions all zero for image ", str(img_names[i]))
+                    if class_category in img_seg and not np.all((attributions[i] == 0)):
+                        valid_indices.append(i)
+
+                print("valid:", valid_indices)
+
+
+                if valid_indices:
+
+                    path = os.path.join(RESULTS_ROOT, class_name)
+                    if not os.path.isdir(path):
+                        os.mkdir(path)
+                    print("Directory '% s' created" % path)
+
+                    y_batch = preds.argmax(axis=1)
+                    
+                    reduced_image_names = np.array(img_names)[valid_indices]
+                    y_batch = y_batch[valid_indices]
+                    x_batch = image[valid_indices]
+                    a_batch = attributions[valid_indices]
+                    reduced_preds = preds[valid_indices]
+
+                    get_resized_binary_mask(reduced_image_names, reduced_preds, class_name, class_category)
+
+                    class_mask_float = get_binary_mask(reduced_preds, class_category)
+                    attributions = get_attributions(model, class_category, class_mask_float, x_batch, layer)
+
+                    get_gradcam_image(reduced_image_names, a_batch, x_batch, class_name, layer_name)
+
+                    scores, histories = irof(model=model,
+                        x_batch=x_batch,
+                        y_batch=y_batch,
+                        a_batch=a_batch,
+                        device=device)
+
+                    if scores is not None:
+                        if class_category not in class_scores.keys():
+                            class_scores[class_category] = []
+                            class_histories[class_category] = []
+
+                        class_scores[class_category].extend(scores)
+                        class_histories[class_category].extend(histories)
         
-        print("mean score:", np.mean(np.array(scores)))
+        all_scores[layer_name] = class_scores
+        all_histories[layer_name] = class_histories
 
-        plot_all_irof_curves_sn(all_histories[layer_name], layer_name)
-        plot_avg_irof_curve_sn(all_histories[layer_name], layer_name)
+        for category in class_scores.keys():
+            class_name = sem_idx_to_class[category]
+            plot_all_irof_curves(class_histories[category], class_name, layer_name)
+            plot_avg_irof_curve(class_histories[category], class_name, layer_name)
 
         with open(os.path.join(RESULTS_ROOT, layer_name, 'histories.pkl'), 'wb') as file:
             pickle.dump(all_histories[layer_name], file)
         
         with open(os.path.join(RESULTS_ROOT, layer_name, 'scores.pkl'), 'wb') as file:
             pickle.dump(all_scores[layer_name], file)
-        
+
     with open(os.path.join(RESULTS_ROOT, 'histories.pkl'), 'wb') as file:
             pickle.dump(all_histories, file)
         
